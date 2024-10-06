1940118f836fa82caecc533471a0dd430314e0a2
LOGESHWARAN R <96953357+Logeshwaranr0902@users.noreply.github.com>
2024-10-06 14:46:10 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: "https://contact-storage-app.onrender.com",
 });
 
 export default instance;
