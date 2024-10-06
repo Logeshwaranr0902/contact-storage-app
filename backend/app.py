09ea879e784ff30ec6dacfe2c559c2c016e58e90
LOGESHWARAN R <96953357+Logeshwaranr0902@users.noreply.github.com>
2024-10-06 14:53:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)  # Enable CORS
+CORS(app, resources={r"/api/*": {"origins": "https://contact-storage-app.netlify.app/"}}) 
+
 db.init_app(app)
 
 with app.app_context():
